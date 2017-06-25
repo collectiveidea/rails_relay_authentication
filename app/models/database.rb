78f35305be679b84de60903c4c9d7bec2dede39f
Jon Stokes <jon@jonstokes.com>
2017-06-25 12:01:38 -0400

    Users and posts from a fixture in the db object

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,3 +1,8 @@
 class Database
-  
+  attr_reader :users, :posts
+
+  def initialize
+    @users = JSON.parse ApplicationController.render(file: Rails.root.join('spec', 'fixtures', 'users.json.erb'))
+    @posts = JSON.parse File.open(Rails.root.join('spec', 'fixtures', 'posts.json'), "r") { |f| f.read }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User
+  # TODO: Replace these with ActiveRecord::Enum
+  ROLES = Struct.new("Roles", :reader, :publisher, :admin).new('reader', 'publisher', 'admin')
+
   def initialize(args)
     @id = args[:id]
     @email = args[:email]

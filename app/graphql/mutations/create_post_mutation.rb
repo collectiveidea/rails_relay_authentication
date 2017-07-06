4948c2e1bc678043356b7df0029a9c2e748ae4ec
Jon Stokes <jon@jonstokes.com>
2017-07-06 11:42:55 -0500

    Got a lot of this working. Need to fix image upload

--- app/graphql/mutations/create_post_mutation.rb
+++ app/graphql/mutations/create_post_mutation.rb
@@ -3,12 +3,36 @@ Mutations::CreatePostMutation = GraphQL::Relay::Mutation.define do
 
   input_field :title, !types.String
   input_field :description, !types.String
-  input_field :image, !types.String
+  input_field :image, !types.String, 'image field is set by upload middleware automatically'
 
-  return_field :postEdge, Types::PostType.connection_type
   return_field :user, Types::UserType
+  return_field :postEdge, Types::PostType.edge_type
   
   resolve ->(object, inputs, ctx) {
-    # Create the post
+    user = ctx[:viewer].user
+
+    new_post = user.posts.build(
+      title: inputs[:title],
+      description: inputs[:description],
+      image: inputs[:image]
+    )
+
+    if new_post.save
+      # Use this helper to create the response that a
+      # client-side RANGE_ADD mutation would expect.
+      range_add = GraphQL::Relay::RangeAdd.new(
+        parent: user,
+        collection: Post.all,
+        item: new_post.reload, # reload picks up the newly generated UUID
+        context: ctx,
+      )
+
+      {
+        user: user,
+        postEdge: range_add.edge,
+      }
+    else
+      GraphQL::ExecutionError.new("#{new_post.errors.messages}")
+    end
   }
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   include CamelizeAttributes
+
+  attr_readonly :uuid 
 end

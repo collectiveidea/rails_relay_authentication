506c19932223b8e277d00af525265270b1466349
Jon Stokes <jon@jonstokes.com>
2017-07-07 18:55:43 -0500

    Added db cleaner

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,21 @@
+require 'capybara/rspec'
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+end

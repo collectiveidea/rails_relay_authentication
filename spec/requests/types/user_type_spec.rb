b0579a0a44233a82aca490771d6903f7bd5863de
Jon Stokes <jon@jonstokes.com>
2017-07-08 15:11:48 -0500

    Green UserType spec

---
+++ spec/requests/types/user_type_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "Types::UserType", type: "request" do
+  let(:endpoint) { "/graphql" }
+  let(:json) { JSON.parse(response.body)["data"] }
+  let!(:viewer) { build(:viewer) }
+  let(:user) { viewer.user }
+  let!(:posts) { create_list(:post, 3, user: user) }
+  let(:user_json) { json["viewer"]["user"] }
+
+  describe "UserType" do
+    let(:query) {
+      <<-GRAPHQL
+        {
+          viewer {
+            user {
+              id
+              email
+              firstName
+              lastName
+              role    
+              posts {
+                edges {
+                  node {
+                    id
+                  }
+                }
+              }                                       
+            }
+          }
+        }
+      GRAPHQL
+    }
+
+    describe "Logged in" do
+      before(:each) {
+        allow_any_instance_of(Warden::Proxy).to receive(:user).and_return(viewer)
+      }
+
+      it "returns a user" do
+        post(endpoint, params: { query: query }  )
+
+        expect(user_json["id"]).to eq(user.uuid)
+        expect(user_json["email"]).to eq(user.email)
+        expect(user_json["firstName"]).to eq(user.first_name)
+        expect(user_json["lastName"]).to eq(user.last_name)
+        expect(user_json["role"]).to eq(user.role)
+        expect(user_json["posts"]["edges"].count).to eq(user.posts.count)
+        expect(user_json["posts"]["edges"].map { |edge| edge["node"]["id"] }).to eq(user.posts.map(&:uuid))
+      end
+    end
+  end
+end

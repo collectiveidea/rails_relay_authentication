5172ae41d874e2cebffdc34f586998df2014b0ca
Jon Stokes <jon@jonstokes.com>
2017-07-15 18:53:06 -0500

    More green

--- spec/requests/types/post_type_spec.rb
+++ spec/requests/types/post_type_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Types::PostType", type: "request" do
   let(:json) { JSON.parse(response.body)["data"] }
   let!(:viewer) { build(:viewer) }
   let(:user) { viewer.user }
-  let!(:user_posts) { create_list(:post, 3, user: user) }
+  let!(:user_posts) { create_list(:post, 3, user_id: user.id) }
   let!(:other_posts) { create_list(:post, 5) }
   let(:user_post) { user_posts.last }
 
@@ -26,7 +26,7 @@ RSpec.describe "Types::PostType", type: "request" do
     }
 
     let(:variables) {{
-      "postId" => user_post.uuid      
+      "postId" => user_post.id      
     }}
 
     shared_examples "returning a post" do
@@ -34,7 +34,7 @@ RSpec.describe "Types::PostType", type: "request" do
         post(endpoint, params: { query: query, variables: variables })
 
         post_json = json["viewer"]["post"]
-        expect(post_json["id"]).to eq(user_post.uuid)
+        expect(post_json["id"]).to eq(user_post.id)
         expect(post_json["title"]).to eq(user_post.title)
         expect(post_json["description"]).to eq(user_post.description)
         expect(post_json["image"]).to eq(user_post.image)

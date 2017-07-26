1ecea4d251774ba033ca467f7008fb12447d28e2
Jon Stokes <jon@jonstokes.com>
2017-07-26 11:28:00 -0500

    Tweak variable name

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_after Rack::ETag, Warden::Manager do |manager|
   manager.failure_app = GraphqlController
 
-  manager.serialize_into_session do |user|
-    user.to_h.slice(:id, :role)
+  manager.serialize_into_session do |viewer|
+    viewer.to_h.slice(:id, :role)
   end
 
   manager.serialize_from_session do |attributes|

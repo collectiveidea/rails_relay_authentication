ad9b39b3f8fe04409674f30e3d1b38e8484856b1
Jon Stokes <jon@jonstokes.com>
2017-07-26 18:28:06 -0500

    Got storybook 3 working

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import NavigationUserMenu from './../client/components/navigation/NavigationUserMenu';
+import StubContainer from "react-storybooks-relay-container"
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -9,3 +12,51 @@ storiesOf('Button', module)
   .add('with some emoji', () => (
     <button onClick={action('clicked')}>😀 😎 👍 💯</button>
   ));
+
+storiesOf('NavigationUserMenu', module)
+.add('can publish', () => {
+  const viewer = {
+    canPublish: true,
+    isLoggedIn: true
+  };
+
+  const props = {
+    viewer: viewer,
+    navigateTo: (target) => action(target)
+  }
+
+  return(
+    <MuiThemeProvider>
+      <StubContainer Component={NavigationUserMenu} props={props} />
+    </MuiThemeProvider>
+  )
+})
+.add('cannot publish', () => {
+  const viewer = {
+    canPublish: false,
+    isLoggedIn: true
+  };
+
+  const props = {
+    viewer: viewer,
+    navigateTo: (target) => action(target)
+  }
+
+  return(
+    <MuiThemeProvider>
+      <StubContainer Component={NavigationUserMenu} props={props} />
+    </MuiThemeProvider>
+  )
+})  .add('is not logged in', () => {
+  const viewer = {}
+
+  const props = {
+    viewer: viewer,
+    navigateTo: (target) => action(target)
+  }
+  return(
+    <MuiThemeProvider>
+      <StubContainer Component={NavigationUserMenu} props={props} />
+    </MuiThemeProvider>
+  )
+});

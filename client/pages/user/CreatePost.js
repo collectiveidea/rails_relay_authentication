cc3586b92642626be9cf4cb3eb0d59650a4332ce
Jon Stokes <jon@jonstokes.com>
2017-08-01 16:35:07 -0500

    Got a basic demo editor working. Long way to go, here

--- client/pages/user/CreatePost.js
+++ client/pages/user/CreatePost.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import { routerShape } from 'found/lib/PropTypes'
@@ -10,6 +12,60 @@ import ImageInput from '../../components/imageInput/ImageInput'
 import CreatePostMutation from '../../mutation/CreatePostMutation'
 
 import styles from './CreatePost.css'
+
+import { Editor, EditorState } from 'draft-js'
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+    this.logState = () => console.log(this.state.editorState.toJS());
+    this.setDomEditorRef = ref => this.domEditor = ref;
+  }
+          
+  componentDidMount(){
+    this.domEditor.focus()
+  }
+  
+  render() {
+    const editStyles = {
+      root: {
+        fontFamily: '\'Helvetica\', sans-serif',
+        padding: 20,
+        width: 600,
+      },
+      editor: {
+        border: '1px solid #ccc',
+        cursor: 'text',
+        minHeight: 80,
+        padding: 10,
+      },
+      button: {
+        marginTop: 10,
+        textAlign: 'center',
+      },
+    };
+
+    return (
+      <div style={editStyles.root}>
+        <div style={styles.editor} onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="Enter some text..."
+            ref={this.setDomEditorRef}
+          />
+        </div>
+        <input
+          onClick={this.logState}
+          style={styles.button}
+          type="button"
+          value="Log State"
+        />
+      </div>
+    );
+  }
+}
 
 class CreatePostPage extends React.Component {
   static propTypes = {
@@ -63,6 +119,8 @@ class CreatePostPage extends React.Component {
     return (
       <div className={styles.content}>
         <h2>Create Post</h2>
+
+        <MyEditor />
 
         <Formsy.Form
           onValid={this.enableButton}
